a49f63ea6f1aa74192d95a60d83b9a4f290717d8
MuhammedSoliu <muhammedsoliu2222@gmail.com>
2024-10-27 15:29:30 +0100

    media query set

--- taskSphere_frontend/src/pages/Boards/BoardBar/BoardBarItems/BoardInvitation.jsx
+++ taskSphere_frontend/src/pages/Boards/BoardBar/BoardBarItems/BoardInvitation.jsx
@@ -2,45 +2,43 @@ import React, { useState, useEffect } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
 import { inviteBoardMember } from '../../../../redux/reducers/inviteSlice';
 import { toast } from 'react-toastify';
+import { FaTimes } from 'react-icons/fa';
 
 const BoardInvitation = ({ board, handleInviteBtn }) => {
   const [email, setEmail] = useState('');
   const dispatch = useDispatch();
-    const { inviteSuccess, inviteError, loading} = useSelector((state) => state.invitation);
+  const { inviteSuccess, inviteError, loading } = useSelector((state) => state.invitation);
 
-  
   useEffect(() => {
-
-    // Show success toast if inviteSuccess is true
-
     if (inviteSuccess) {
       toast.success('Invitation sent successfully!', { position: "top-right" });
-
-      handleInviteBtn(false)
-
-    }
-    else if (inviteError) {
+      handleInviteBtn(false);
+    } else if (inviteError) {
       toast.error(inviteError, { position: "top-right" });
     }
-  },[inviteSuccess, inviteError]);
+  }, [inviteSuccess, inviteError]);
 
-  const handleInvite =  (e) => {
-    e.preventDefault()
+  const handleInvite = (e) => {
+    e.preventDefault();
     const boardId = board._id;
     const inviteData = { email: email.toLowerCase() };
 
     if (inviteData?.email.includes("@")) {
-       // Check if email is valid
       dispatch(inviteBoardMember({ boardId, inviteData }));
     } else {
       toast.error("Enter a valid email!", { position: "top-right" });
     }
   };
 
-
-
   return (
-    <form onSubmit={handleInvite} className="absolute top-0 right-0 mt-5 mr-5 p-4 bg-white shadow-lg rounded-lg w-80 md:w-96 z-10 text-center">
+    <form onSubmit={handleInvite} className="relative mt-5 p-4 bg-white shadow-lg rounded-lg w-80 md:w-96 z-10 text-center">
+      <button 
+        type="button" 
+        className="absolute top-2 right-2 text-gray-500 hover:text-gray-700"
+        onClick={() => handleInviteBtn(false)}
+      >
+        <FaTimes size={20} />
+      </button>
       <input
         type="email"
         required
@@ -49,13 +47,12 @@ const BoardInvitation = ({ board, handleInviteBtn }) => {
         value={email}
         onChange={(e) => setEmail(e.target.value)}
       />
-      <button className="w-[50%] p-2 bg-blue-400 text-white rounded-md hover:bg-blue-500 transition-colors duration-200"
-      >
+      <button className="w-[50%] p-2 bg-blue-400 text-white rounded-md hover:bg-blue-500 transition-colors duration-200">
         {loading ? (
-                    <div className="spinner"></div>
-                  ) : (
-                    "Invite"
-                  )}
+          <div className="spinner"></div>
+        ) : (
+          "Invite"
+        )}
       </button>
     </form>
   );
