029612646568f643f34c23bf33d0860e7787a0dc
MuhammedSoliu <muhammedsoliu2222@gmail.com>
2024-10-29 12:05:45 +0100

    update

--- taskSphere_frontend/src/components/dashboard/MainContent.jsx
+++ taskSphere_frontend/src/components/dashboard/MainContent.jsx
@@ -103,7 +103,7 @@ const MainContent = () => {
                                     <label htmlFor="">Search:</label>
                                     <input
                                         type="text"
-                                        className="border outline-none border-gray-300 rounded py-[3px] "
+                                        className="border outline-none border-gray-300 rounded py-[3px] w-2 "
                                         placeholder="Search by title"
                                         value={searchTerm}
                                         onChange={(e) => setSearchTerm(e.target.value)}
