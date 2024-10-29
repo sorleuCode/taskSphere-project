1caa1d385fb62f829acb9a35cf47ee2ec6ede29b
MuhammedSoliu <muhammedsoliu2222@gmail.com>
2024-10-29 15:47:43 +0100

    update

--- taskSphere_frontend/src/redux/store/store.js
+++ taskSphere_frontend/src/redux/store/store.js
@@ -5,12 +5,23 @@ import userReduder from "../reducers/userSlice"
 import columnReducer from "../reducers/columnSlice"
 import inviteReducer from "../reducers/inviteSlice"
 import StreamClientTokenReducer from "../reducers/videoSlice"
+import { persistReducer } from 'redux-persist'
+import storage from 'redux-persist/lib/storage'
+
+const persistConfig = {
+    key: 'root',
+    storage
+};
+
+const persistedUserReducer = persistReducer(persistConfig, userReduder);
+
+
 
 export const store = configureStore({
     reducer: {
         board: boardReducer,
         card: cardReducer,
-        user: userReduder,
+        user: persistedUserReducer,
         column: columnReducer,
         invitation: inviteReducer,
         clientToken: StreamClientTokenReducer
